f1e0bcf72073c67b9de3b2fd6fc218bc026d69fe
fadil134 <130072912+fadil134@users.noreply.github.com>
2024-01-14 16:16:38 +0700

    modified:   application/config/database.php modified:   application/views/dist/_partials/footer.php deleted:    i4te

--- application/views/dist/_partials/footer.php
+++ application/views/dist/_partials/footer.php
@@ -1,6 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
+<!--
       <footer class="main-footer">
         <div class="footer-left">
           Copyright &copy; 2018 <div class="bullet"></div> Design By <a href="https://nauval.in/">Muhamad Nauval Azhar</a>
@@ -9,6 +10,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
           
         </div>
       </footer>
+-->
     </div>
   </div>
 
